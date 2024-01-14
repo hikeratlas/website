ed13fd69b6988e5519aa89df65733c500b72c1c8
Colin Dellow <cldellow@gmail.com>
2024-01-13 22:13:37 -0500

    test auto deploy

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,7 +11,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Hiker Atlas</h1>
-      <p>Hi! I'm <a href="https://cldellow.com/">Colin</a>. This is a hobby project, so there's not much here yet. You might want to check out:</p>
+      <p>Hey! I'm <a href="https://cldellow.com/">Colin</a>. This is a hobby project, so there's not much here yet. You might want to check out:</p>
       <ul>
         <li>
           <a
